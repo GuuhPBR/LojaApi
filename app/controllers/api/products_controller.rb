0381100ff032a996a826a87754dc40f963dabac5
Gustavo Pinheiro <RM89053@fiap.com.br>
2022-11-21 22:31:40 -0300

    Adiciona permissao pro nrok

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,4 +1,6 @@
 class Api::ProductsController < ApplicationController
+    protect_from_forgery with: :null_session
+    skip_before_action :verify_authenticity_token
     before_action :set_product, only: %i[ show update destroy ]
   
     def index
